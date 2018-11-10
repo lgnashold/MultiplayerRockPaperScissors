348afea8ff4c84ca8f9f83ce9e095b0234089228
Anna AD <anna.arpacidusseau@gmail.com>
2018-11-09 19:19:11 -0600

    Python now works with new database structure

--- rps/matchmaking.py
+++ rps/matchmaking.py
@@ -9,14 +9,24 @@ bp = Blueprint('matchmaking', __name__)
 @bp.route("/", methods = ('GET','POST'))
 def index():
     if request.method == "POST":
+        nickname = request.form['nickname']
+        join_code = request.form['join_code']
+
         db = get_db()
-        db.execute(
-            'INSERT INTO game (move1,move2) VALUES (?,?)',(0,0)
-        )
-        db.commit()
+
+        game_result = db.execute('SELECT id,name1,name2 FROM game WHERE joincode = ?', (join_code,)).fetchone()
+
+        print(game_result)
+
+        if game_result is None:
+            #create new game
+            db.execute(
+                'INSERT INTO game (joincode,name1) VALUES (?,?)',(join_code,nickname)
+            )
+            db.commit()
+
+        else:
+            #join exsisting game
+            pass
 
     return render_template("index.html")
-
-@bp.route("/join", methods = ('GET','POST'))
-def join():
-    return render_template("join.html")
